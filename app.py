842ed4ae421e7559316041eb65dc175c03e5f83e
malavneha <malavneha855@gmail.com>
2026-08-21 07:58:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    confusion_matrix
+)
 
 st.set_page_config(
     page_title="AI Clinical Decision Support System",
